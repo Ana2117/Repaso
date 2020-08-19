2f889b61caaed1d00bdcb7b8ad688284f2145ce8
Ana Carolina <anacarolina2117@hotmail.com>
2020-08-19 12:12:12 +0200

    first commit

---
+++ SignoZodiacal.ts
@@ -0,0 +1,35 @@
+let mes : 12 
+let SignoZ: number 
+
+switch (mes) {
+    case 1:
+        // Enero
+        console.log("Acuario");
+        break;
+    case 2:
+        // Febrero 
+        console.log("It is a Monday.");
+        break;
+    case 3:
+        // Marzo
+        console.log("It is a Tuesday.");
+        break;
+    case 4:
+        //Abril
+        console.log("It is a Wednesday.");
+        break;
+    case 5:
+        // Mayo
+        console.log("It is a Thursday.");
+        break;
+    case 6:
+        // Junio 
+        console.log("It is a Friday.");
+        break;
+    case 7:
+        console.log("It is a Saturday.");
+        break;
+    default:
+        console.log("No such day exists!");
+        break;
+}
